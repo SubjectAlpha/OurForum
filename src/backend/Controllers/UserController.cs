e87844646ce04089a3ecb63389f9adda4d29ec57
Jake Starr <jakestarr60@gmail.com>
2024-05-04 23:35:44 -0400

    user registration/login

--- src/backend/Controllers/UserController.cs
+++ src/backend/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 using System.Diagnostics;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using backend.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OurForum.Backend.Entities;
 using OurForum.Backend.Services;
+using OurForum.Backend.Utility;
 
 namespace OurForum.Backend.Controllers;
 
@@ -28,8 +31,11 @@ public class UserController : Controller
 
     [HttpPost("authenticate")]
     [AllowAnonymous]
-    public IActionResult Authenticate([FromBody] TokenGenerationRequest body)
+    public IActionResult Authenticate([FromBody] LoginRequest body)
     {
+        var validationResult = LoginRequestValidation(body);
+        if (!validationResult.Success) { return BadRequest("Email/Password validation failed"); }
+
         var token = AuthService.Authenticate(body.Email, body.Password);
         if (!string.IsNullOrEmpty(token))
         {
@@ -41,25 +47,61 @@ public class UserController : Controller
 
     [HttpPost("register")]
     [AllowAnonymous]
-    public IActionResult Register([FromBody] TokenGenerationRequest body)
+    public IActionResult Register([FromBody] RegistrationRequest body)
     {
-        if (string.IsNullOrWhiteSpace(body.Email) || string.IsNullOrWhiteSpace(body.Password))
+        var validationResult = LoginRequestValidation(body);
+        if (
+            !validationResult.Success
+            || !RegexValidation.ValidatePassword(body.ConfirmPassword)
+            || !body.Password.Equals(body.ConfirmPassword)
+        )
         {
-            return BadRequest("Username and password cannot be blank");
+            return BadRequest("Email/Password validation failed");
         }
 
-        // validate email and password further here
+        var newUser = IUserService.Create(body.Email, body.Password);
 
-        IUserService.Create(body.Email, body.Password);
-        return Authenticate(body);
+        if (newUser != null)
+        {
+            return Authenticate(body);
+        }
+        return StatusCode(500, "Failed to create user");
     }
 
-    public struct TokenGenerationRequest
+    private static ServiceResponse LoginRequestValidation(LoginRequest body)
+    {
+        var response = new ServiceResponse();
+        if (string.IsNullOrWhiteSpace(body.Email) || string.IsNullOrWhiteSpace(body.Password))
+        {
+            response.Errors.Add("Username and password cannot be blank");
+        }
+
+        if (!RegexValidation.ValidateEmail(body.Email))
+        {
+            response.Errors.Add("Email validation failed");
+        }
+
+        if (!RegexValidation.ValidatePassword(body.Password))
+        {
+            response.Errors.Add("Password validation failed");
+        }
+
+        response.Success = response.Errors.Count > 0;
+        return response;
+    }
+
+    public class LoginRequest
     {
         [JsonPropertyName("email")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [JsonPropertyName("password")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class RegistrationRequest : LoginRequest
+    {
+        [JsonPropertyName("confirmPassword")]
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
---
+++ src/backend/Utility/RegexValidation.cs
@@ -0,0 +1,19 @@
+namespace backend.Utility;
+
+public static class RegexValidation
+{
+    private static readonly string EmailRegex =
+        @"^(?=.{1,254}$)(?:(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*)|(?:""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*""))@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)$";
+    public static readonly string PasswordRegex =
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$";
+
+    public static bool ValidateEmail(string email)
+    {
+        return System.Text.RegularExpressions.Regex.IsMatch(email, EmailRegex);
+    }
+
+    public static bool ValidatePassword(string password)
+    {
+        return System.Text.RegularExpressions.Regex.IsMatch(password, PasswordRegex);
+    }
+}
