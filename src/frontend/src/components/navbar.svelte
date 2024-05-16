88490a6df6c9bfde821dab7d0f65375d30c24f2e
Jake Starr <jakestarr60@gmail.com>
2024-05-15 22:55:10 -0400

    svelte + tailwind <3

---
+++ src/frontend/src/components/navbar.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let title;
+</script>
+
+
+<nav class="w-100 p-2 bg-gray-300 dark:bg-slate-700">
+    <h1 class="ml-2 text-xl text-bolt text-black dark:text-white">{title}</h1>
+</nav>
---
+++ src/frontend/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../app.css";
+    import Navbar from "../components/navbar.svelte";
+
+    console.log(import.meta.env.VITE_TITLE);
+
+    const title = import.meta.env.VITE_TITLE;
+</script>
+
+<div class="bg-white dark:bg-slate-800 h-screen overflow-y-hidden overflow-x-hidden">
+    <Navbar title={title} />
+    <slot />
+</div>
+
+
