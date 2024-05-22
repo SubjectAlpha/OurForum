ff4a2a9a977222e44953a19faa747f738051d1b9
Jake Starr <jakestarr60@gmail.com>
2024-05-21 22:53:39 -0400

    learning the svelte ways

--- src/frontend/src/routes/login/+page.svelte
+++ src/frontend/src/routes/login/+page.svelte
@@ -1,48 +1,56 @@
 <script>
 	// on validation failed
 
+	let email = "";
+    let password = "";
+
+    let emailRegex = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+    let passwordRegex = new RegExp("^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$");
+
+    $: emailValid = true;
+    $: passwordValid = true;
+
+    async function submitForm(){
+        emailValid = emailRegex.test(email);
+        passwordValid = passwordRegex.test(password);
+    }
 </script>
 
-<div class="p-12">
+<div class="p-12 pt-0 grid h-screen place-items-center">
 	<form
-		class="bg-white dark:bg-slate-600 shadow-md rounded px-8 pt-6 pb-8 mb-4"
-		on:submit={(e) => {
-			e.preventDefault();
-            var email = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
-
-            if(password == "" || password.length < 4)
-            {
-                document.getElementById('password')?.classList.toggle('border-red-500');
-	            document.getElementById('password-validation')?.classList.toggle('hidden');
-            }
-
-            console.log(email);
-		}}
+		class="bg-white dark:bg-slate-600 shadow-md rounded p-8 mb-4 lg:w-1/3 md:w-1/3 sm:w-full"
+		on:submit|preventDefault={submitForm}
 	>
 		<div class="mb-4">
 			<label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="email">
-				Email
-			</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				id="email"
-				type="email"
-				placeholder="Email"
-			/>
+                Email Address
+            </label>
+
+            <input
+                bind:value={email}
+                class:border-red-500={!emailValid}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="john.smith@email.com"
+            />
+			<p class:hidden={emailValid} class="text-red-500 text-xs italic hidden">
+				Please enter an email.
+			</p>
 		</div>
 		<div class="mb-6">
-			<label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="password">
-				Password
-			</label>
-			<input
-				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-				id="password"
-				type="password"
-				placeholder="******************"
-			/>
-			<p id="password-validation" class="text-red-500 text-xs italic hidden">
-				Please choose a password.
+            <label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="password">
+                Password
+            </label>
+
+            <input
+                bind:value={password}
+                class:border-red-500={!passwordValid}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="password"
+                placeholder={"******************"}
+            />
+			<p class:hidden={passwordValid} class="text-red-500 text-xs italic hidden">
+				Please enter a password.
 			</p>
 		</div>
 		<div class="flex items-center justify-between">
