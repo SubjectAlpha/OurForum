bf10fdbfa1fd8c8080268854dee85eb39e37f6d9
Jake Starr <jakestarr60@gmail.com>
2024-11-11 22:15:11 -0500

    messing w index views

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,15 +1,18 @@
 <script>
-    import "../app.css";
-    import Navbar from "../components/navbar.svelte";
+	import '../app.css';
+	import Navbar from '../components/navbar.svelte';
 
-    const title = import.meta.env.VITE_TITLE;
-    console.log("title", title);
-    $: title;
+	const title = import.meta.env.VITE_TITLE;
+	console.log('title', title);
+	$: title;
 </script>
 
-<div class="bg-white dark:bg-slate-800 dark:text-white h-screen overflow-y-hidden overflow-x-hidden">
-    <Navbar title={title} />
-    <main class="bg-gray-50 dark:bg-slate-600 m-4 p-1 h-full rounded">
-        <slot />
-    </main>
+<div
+	class="bg-white dark:bg-slate-800 dark:text-white
+		h-screen overflow-y-hidden overflow-x-hidden"
+>
+	<Navbar {title} />
+	<main class="bg-gray-50 dark:bg-slate-600 m-2 p-1 pb-10 lg:pb-14 h-full rounded overflow-y-auto">
+		<slot />
+	</main>
 </div>
